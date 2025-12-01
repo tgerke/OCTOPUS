48586308038a68f2cec383efbf558492c9d390c0
Travis Gerke <tgerke@mail.harvard.edu>
2025-12-01 12:15:02 -0800

    Update package metadata and regenerate documentation with roxygen2 7.3.3
    When devtools::build() was run, it triggered roxygen2::roxygenize() which:
    
    1. Updated RoxygenNote from 7.3.1 to 7.3.3 (matching installed version)
    2. Removed manually-added importFrom statements from NAMESPACE (since they
       were not tracked in R source files)
    3. Generated documentation for @note tags added to RunAnalysis functions
    
    To fix this and maintain the imports properly:
    - Created R/OCTOPUS-package.R with @importFrom tags for all base R functions
    - This ensures roxygen2 will regenerate the imports automatically
    - Generated new man/OCTOPUS-package.Rd documentation
    - Updated man/OCTOPUS.Rd (removed duplicate alias)
    - Added documentation for confint exclusion note
    
    This is the proper roxygen2 workflow: imports should be documented with
    @importFrom tags in R source files, not manually edited in NAMESPACE.
    
    Files changed:
    - DESCRIPTION: RoxygenNote 7.3.1 -> 7.3.3
    - NAMESPACE: Regenerated with importFrom statements (proper format)
    - R/OCTOPUS-package.R: New package documentation with import declarations
    - man/OCTOPUS-package.Rd: New auto-generated documentation
    - man/OCTOPUS.Rd: Updated to remove duplicate alias
    - man/RunAnalysis.RankedExact.Rd: Added confint note documentation

---
+++ R/OCTOPUS-package.R
@@ -0,0 +1,20 @@
+##### COPYRIGHT #############################################################################################################
+#
+# Copyright (C) 2018 JANSSEN RESEARCH & DEVELOPMENT, LLC
+# This package is governed by the JRD OCTOPUS License, which is the
+# GNU General Public License V3 with additional terms. The precise license terms are located in the files
+# LICENSE and GPL.
+#
+#############################################################################################################################.
+
+#' @keywords internal
+#' @importFrom grDevices dev.off pdf
+#' @importFrom graphics abline legend lines text title
+#' @importFrom methods new
+#' @importFrom stats coef lm na.omit predict pt qnorm qpois qt quantile rexp rgamma rmultinom rnorm runif t.test update vcov wilcox.test
+#' @importFrom utils getS3method read.table write.table
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
